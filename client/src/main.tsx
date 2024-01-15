3485cf5760c7778eda2a08b5ccc0424e953029c9
lynxx007 <luthfirizky7@gmail.com>
2024-01-15 21:48:24 +0700

    setup all providers

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { CSSReset, ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+import { persistor, store } from "./app/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
+import theme from "./styles/theme.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ChakraProvider theme={theme}>
+          <CSSReset />
+          <App />
+        </ChakraProvider>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
