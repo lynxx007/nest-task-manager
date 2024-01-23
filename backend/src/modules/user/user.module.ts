0bdca8f33e838b38f8df000379ea2069bb4f3414
lynxx007 <luthfirizky7@gmail.com>
2024-01-23 09:58:28 +0700

    created 2 middlewares for schema

--- backend/src/modules/user/user.module.ts
+++ backend/src/modules/user/user.module.ts
@@ -3,10 +3,36 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schema/user.schema';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-
+import bcrypt from 'bcrypt';
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeatureAsync([
+      {
+        name: User.name,
+        useFactory: () => {
+          const schema = UserSchema;
+          schema.pre('save', async function () {
+            if (!this.isModified('password')) return;
+
+            this.password = await bcrypt.hash(this.password, 10);
+          });
+          return schema;
+        },
+      },
+      {
+        name: User.name,
+        useFactory: () => {
+          const schema = UserSchema;
+          schema.set('toJSON', {
+            transform: function (doc, ret) {
+              delete ret.password;
+              return ret;
+            },
+          });
+          return schema;
+        },
+      },
+    ]),
   ],
   exports: [MongooseModule, UserService],
   controllers: [UserController],
