f7d33d01b95cbc24463ddec6f08a26137baf1645
lynxx007 <luthfirizky7@gmail.com>
2024-01-24 19:22:30 +0700

    added more properties CreateTaskDto

--- backend/src/modules/tasks/dto/task.dto.ts
+++ backend/src/modules/tasks/dto/task.dto.ts
@@ -1,9 +1,17 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { MinLength } from 'class-validator';
+import { IsDateString, MinLength } from 'class-validator';
 
 export class CreateTaskDto {
   @MinLength(1, { message: 'Title must be at least 1 characters' })
   title: string;
+
+  @IsDateString()
+  to: Date;
+
+  @IsDateString()
+  from: Date;
+
+  members?: string[];
 }
 
 export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
